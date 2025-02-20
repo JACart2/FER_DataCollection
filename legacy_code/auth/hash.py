e45a946d4e3b9276727486a51477b16000adc955
nicknewn <91585872+nicknewn@users.noreply.github.com>
2025-02-20 17:07:42 -0500

    Simple password encryption in tkinter window

--- legacy_code/auth/hash.py
+++ legacy_code/auth/hash.py
@@ -1,5 +1,16 @@
 import hashlib
 
-m = hashlib.sha256()
-m.update(b"this is a test run")
-print(m.digest())
+# Add auth0 to presentation for next sprint
+# Implement mysql password comparison
+def hashPassword(password):
+    convert_to_bytes = str.encode(password)
+
+    hash_password = hashlib.sha256()
+    hash_password.update(convert_to_bytes)
+    print(hash_password.digest())
+    # test if they are the same password
+    # m = hashlib.sha256()
+    # m.update(b"this is a test run")
+    # print(m.digest())
+
+    return hash_password.digest()
