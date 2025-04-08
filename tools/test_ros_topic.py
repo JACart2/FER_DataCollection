73997c44f25f5035dc07ea4054cce00d2e770c83
Gr8jj28 <gr8jj28@gmail.com>
2025-04-08 14:41:07 -0400

    updating test ros

--- tools/test_ros_topic.py
+++ tools/test_ros_topic.py
@@ -3,7 +3,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
-
+ 
 class ZedImageSubscriber(Node):
     def __init__(self):
         super().__init__('zed_image_subscriber')
