0ae9172ccb4ce5298e5af2b481a2854044422385
nicknewn <91585872+nicknewn@users.noreply.github.com>
2025-04-26 20:31:30 -0400

    Update window.py

--- modified_legacy_code/auth/window.py
+++ modified_legacy_code/auth/window.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
 import hash
-
 from pyzed import sl
 import numpy as np
 import cv2
@@ -13,16 +12,10 @@ def popupAuthWindow():
     win = tk.Toplevel()
     global password_input 
     password_input = tk.StringVar()
-
-
-def popupAuthWindow():
-    win = tk.Toplevel()
-
     win.wm_title("Auth Window")
     win.geometry("250x250")
     text_field = tk.Entry(win, width=30, text="Password", textvariable=password_input) #, show="*"
     text_field.pack(pady=10)
-
   
     submit_button = tk.Button(win, text="Submit", command=lambda: submit(password_input.get()))
     submit_button.pack()
@@ -44,30 +37,3 @@ def submit(password_input):
     else:
         print(str(ValueError) + " Passwords are different")
         password_auth = False
-
-    submit_button = tk.Button(win, text = "Submit", command=submit)
-    submit_button.pack()
-
-def submit():
-    user_input = password_input.get()
-    hashcode = hash.hashPassword(user_input)
-    print("The hashcoded password is: " + str(hashcode))
-
-root = tk.Tk()
-password_input = tk.StringVar()
-root.geometry("500x500")
-label = tk.Label(root, text="Hello, Tkinter!")
-label.pack()
-
-
-authenticate_button = tk.Button(root, text="Scan Face", command=popupAuthWindow)
-authenticate_button.pack()
-
-# scan_face_button = tk.Button(root, text="Scan Face", command=popupAuthWindow)
-# scan_face_button.pack()
-
-# help_button = tk.Button(root, text="help", command=test)
-# help_button.pack()
-
-root.mainloop()
-
