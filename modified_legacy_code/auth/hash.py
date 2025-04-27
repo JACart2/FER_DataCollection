7a4798e20d86526a63ce6e71652c4eccdbf20576
nicknewn <91585872+nicknewn@users.noreply.github.com>
2025-04-26 20:31:05 -0400

    Update hash.py

--- modified_legacy_code/auth/hash.py
+++ modified_legacy_code/auth/hash.py
@@ -1,6 +1,5 @@
 import hashlib
 from argon2 import PasswordHasher
-
 import database as db
 
 # Implements mysql password comparison
@@ -33,20 +32,3 @@ def hashPassword(password_input):
     curs.close()
     connection.close()
     return bool_statement
-
-
-# Implement mysql password comparison
-def hashPassword(password_input):
-    ph = PasswordHasher()
-    hash_password = ph.hash(password=password_input)
-    print("The password is now hashed as: ", hash_password)
-    temp_password = "Test"
-    
-    try:
-        ph.verify(hash_password, temp_password)
-        print("They are the same")
-    except:
-        print("Passwords are different")
-
-    return hash_password
-
